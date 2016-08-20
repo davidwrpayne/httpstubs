54884bb0eab0864063f139b7dae2eda2b4f5ccb0
Brent Rich <bdrich.mail@gmail.com>
2016-08-20 10:41:53 -0700

    Add handcrafted example cassette.
    The goal of the server is to respond back with stubs. An easy way
    to represent stubs in a file is to build off the replay part of
    go-vcr. This commit adds a handcrafted cassette that demonstrates
    this functionality in the test script.
    
    Unrelatedly, I'm trying out a different coding style for errors. The
    typical way I've been writing error blocks looks like:
    
    ```
    x, err := statement()
    if err != nil {
      // handle error
    }
    
    do(x)
    ```
    
    However, I've noticed that there are subtle downsides to declaring the
    error outside the error handling scope: (1) mistakes in error handling
    like failing to return can cause previous errors to trigger the wrong
    block and (2) error handling blocks that don't key off the error type
    can accidentally call err.Error() on nil, leading to a panic. The
    style in this commit avoids this by only declaring errors within if
    blocks.
    
    ```
    var x T
    if w, err := statement(); err != nil {
      // handle error
    } else {
      x = w
    }
    
    do(x)
    ```
    
    This ensures that the compiler will catch any undeclared or nil errors
    and won't send them forward to trigger the wrong error block. It tends
    towards a more immutable style of go programming.

--- httpstubs.go
+++ httpstubs.go
@@ -1,11 +1,42 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/dnaeon/go-vcr/recorder"
+	"os"
+	"fmt"
+	"io"
+)
 
 func main() {
+	file := "example"
+
+	if _, err := os.Stat(fmt.Sprintf("%s.yaml", file)); os.IsNotExist(err) {
+		// missing example file should halt server boot.
+		panic(err)
+	}
+
+	var vcr *recorder.Recorder
+	if rec, err := recorder.New("example"); err != nil {
+		// invalid cassette should halt server boot.
+	 	panic(err)
+	} else {
+		vcr = rec
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		w.Write([]byte(`hello httpstubs`))
+		if response, err := vcr.Transport.RoundTrip(r); err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "error: %s", err)
+		} else {
+			w.WriteHeader(response.StatusCode)
+			for k, v := range response.Header {
+				for _, i := range v {
+					response.Header.Add(k, i)
+				}
+			}
+			io.Copy(w, response.Body)
+		}
 	})
 	http.ListenAndServe("0.0.0.0:3000", nil)
 }
--- tests.sh
+++ tests.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+set -e
 go install github.com/brentdrich/httpstubs
 httpstubs &
 PID=$!
-trap "kill -9 $PID" INT TERM
-curl -XGET -v 0.0.0.0:3000/
+trap "kill $PID" INT TERM
+sleep 1 # some time to boot
+
+echo "Test 1 - Included in cassette:"
+curl -XGET -v 0.0.0.0:3000/hello
+echo
+echo "----------"
+echo "Test 2 - Not Included in cassette:"
+curl -XGET -v 0.0.0.0:3000/goodbye
 echo
